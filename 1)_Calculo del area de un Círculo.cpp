b95e6fe354478efa96a39f44e0f1aac3bf65ad5c
dharien-llanos <dharien1317@gmail.com>
2024-06-22 23:25:46 -0500

    agrege el primer ejercicio de funciones ,calculo del area de un circulo

---
+++ 1)_Calculo del area de un Círculo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+	double r, area;
+	cout<<"Introdusca  el radio del circulo: ";
+	cin>>r;
+	area=M_PI*pow(r,2);
+	cout<<"El area del circulo  es:  "<<area;
+	
+	return 0;
+}
